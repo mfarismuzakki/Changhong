bedbcea5f0f0ccbb6810de43bc11aa7f96625a81
mfarismuzakki <34634822+mfarismuzakki@users.noreply.github.com>
2018-04-01 16:53:16 +0700

    layout

---
+++ application/controllers/HomePage.php
@@ -0,0 +1,29 @@
+<?php 
+	defined('BASEPATH') OR exit('No direct script access allowed');
+
+	class HomePage extends CI_Controller{
+
+		function construct(){
+			parrent::__construct();
+			$this->load->helper('url');
+		}
+
+
+		function index(){
+			$this->load->view('index');
+		}
+
+
+		function Register(){
+
+			
+
+
+		}
+
+
+	}
+
+
+
+ ?>
